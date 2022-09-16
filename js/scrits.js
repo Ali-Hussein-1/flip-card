30ac7398fba2bcafee69fa75115e8f56459626ef
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-16 15:52:11 +0300

    Major modifying for styling and index

--- js/scrits.js
+++ js/scrits.js
@@ -1,11 +1,1 @@
-const card = document.querySelector("#c1")
-card.addEventListener("click",fct1)
-function fct1()
-{
-    card.classList.toggle("flip")
-}
 
-
-// for (let i=0; i<=cards.length; i++){
-    
-// }
