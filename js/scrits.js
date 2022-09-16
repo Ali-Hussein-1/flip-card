53a589bb081fffb19a7f1ab93d6549e7b3020e85
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-16 17:32:56 +0300

    Adding game functionality and fixed some bugs

--- js/scrits.js
+++ js/scrits.js
@@ -3,3 +3,46 @@ let counter = 0;
 let firstSelection = "";
 let secondSelection = "";
 
+const cards = document.querySelectorAll(".cards .card");
+cards.forEach((card) => {
+  // addding an event upon clicking 
+  card.addEventListener("click", () => {
+    // changing card styling upon clicking
+    card.classList.add("clicked");
+    // checking the number of selected cards
+    if (counter === 0) {
+      firstSelection = card.getAttribute("lang");
+      counter++;
+    } 
+    else {
+      secondSelection = card.getAttribute("lang");
+      counter = 0;
+      // checking if the chosen images are the same 
+      if (firstSelection === secondSelection) {
+        const correctCards = document.querySelectorAll(
+          ".card[lang='" + firstSelection + "']"
+        );
+          // changing the styling of matching cards
+        correctCards[0].classList.add("checked");
+        correctCards[0].classList.remove("clicked");
+        correctCards[1].classList.add("checked");
+        correctCards[1].classList.remove("clicked");
+      } 
+      else {
+        const incorrectCards = document.querySelectorAll(".card.clicked");
+        // changing the styling of non-matching cards
+        incorrectCards[0].classList.add("shake");
+        incorrectCards[1].classList.add("shake");
+        // A reset function for non-matching cards
+        function reset() {
+          incorrectCards[0].classList.remove("shake");
+          incorrectCards[0].classList.remove("clicked");
+          incorrectCards[1].classList.remove("shake");
+          incorrectCards[1].classList.remove("clicked");
+        }
+        // setting a reset timer
+        setTimeout(reset,1000);
+      }
+    }
+  });
+});
